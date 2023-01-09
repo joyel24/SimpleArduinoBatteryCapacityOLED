3e8fd4c2f61fa31cbb978cf67758e4fab6d0a257
joyel24 <joel.martin.2104@gmail.com>
2023-01-09 22:28:28 +0100

    Show voltage from ADC0

--- SimpleArduinoBatteryCapacity.ino
+++ SimpleArduinoBatteryCapacity.ino
@@ -15,21 +15,47 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+#define NUM_OF_AVERAGE_ATTEMPTS 200     //Number of values picked for average adc calculation
+#define MEASURED_VCC_REF 4.6            //Measured voltage accross ground and Vcc pin of your arduino (it can depends on its supply)
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.clearDisplay();
 
+  // Display Welcome Message during 3sec
   display.setTextSize(3);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
   display.println("Welcome");
   display.display();
-  delay(2000);
+  delay(3000);
+}
+
+int getadcValue(){
+  uint32_t adcValuesTotal = 0;
+  int averageValue = 0;
+  for (int n = 0; n < NUM_OF_AVERAGE_ATTEMPTS; n++){ //Averaging values for n times attempts
+    adcValuesTotal += analogRead(A0);
+  }
+  return adcValuesTotal / NUM_OF_AVERAGE_ATTEMPTS;
+}
+
+float getADCValToVolts(){
+  float value = getadcValue();
+  return value / 1024 * MEASURED_VCC_REF;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  //Print Voltage Measured from cell connected to ADC0
+  display.setCursor(0, 0);
+  display.print(getADCValToVolts()); display.print("V");
+  display.display();
+  //delay(500);
 
 }
