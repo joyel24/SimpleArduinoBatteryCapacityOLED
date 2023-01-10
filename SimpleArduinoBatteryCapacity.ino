ba10441ebeb2be40c861bbb9aedd5a6c92c0e6ef
joyel24 <joel.martin.2104@gmail.com>
2023-01-10 07:28:30 +0100

    Consider voltage drop + Implement mAh counter

--- SimpleArduinoBatteryCapacity.ino
+++ SimpleArduinoBatteryCapacity.ino
@@ -15,12 +15,14 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define NUM_OF_AVERAGE_SAMPLES 2000     //Number of values picked for average adc calculation
+#define NUM_OF_AVERAGE_SAMPLES 500     //Number of values picked for average adc calculation
 #define MEASURED_VCC_REF 4.75           //Measured voltage accross ground and Vcc pin of your arduino (it can depends on its supply)
+#define MEASURED_VCC_WITH_LOAD 4.67     //
 #define SHUNT_RESISTOR_OHM 1.00         //Value of Shunt Resistor (Ohm) to measure intensity of discharge
 #define PIN_RELAY_OR_MOSFET 7           //PIN used to start or stop the discharge 
 #define PIN_RELAY_DEFAULT 0             //Not Used yet
 #define PIN_BUTTON 5                    //Pin of the button to start/stop discharge
+#define STOP_DISCHARGE_VOLTAGE 3.2     //
 
 void setup() {
   // put your setup code here, to run once:
@@ -52,7 +54,10 @@ int getadcValue(int pin){
 
 float getVoltage(int pin){
   float value = getadcValue(pin);
+  if (digitalRead(PIN_RELAY_OR_MOSFET) == LOW){
   return value / 1023 * MEASURED_VCC_REF;
+  }
+  else {return value / 1023 * MEASURED_VCC_WITH_LOAD;}
 }
 
 float getIntensity(int pin){
@@ -67,7 +72,7 @@ float getPower(){
   return power;
 }
 
-void DisplayInstant(){
+void DisplayRefresh(uint32_t SecondsElapsed, float mAh){   //Function to display instant voltage, intensity and power on 1st line & other function parameters
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -75,6 +80,9 @@ void DisplayInstant(){
   display.print(getVoltage(A0)); display.print("V ");     //Display Voltage
   display.print(getIntensity(A1)); display.print("A ");   //Display Intensity
   display.print(getPower()); display.print("W ");         //Display Power
+  display.setCursor(0, 9);
+  display.print(SecondsElapsed);; display.print("s ");    //Timer
+  display.print(mAh);; display.print("mAh ");
   display.display();
 }
 
@@ -83,17 +91,23 @@ void DisplayInstant(){
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(PIN_RELAY_OR_MOSFET, LOW);
-  DisplayInstant();
-  //int resetTime = millis();
+  float mAs = 0;
+  float mAh = mAs/3600;
+  int millisecFormAsInterval = 5000;
+  DisplayRefresh(0, 0);
   if (digitalRead(PIN_BUTTON) == HIGH){
     uint32_t resetTime = millis();
-    digitalWrite(PIN_RELAY_OR_MOSFET, HIGH);
-    while (digitalRead(PIN_BUTTON) == HIGH){
-      DisplayInstant();
-      display.setCursor(0, 9);
+    uint32_t startMillisFormAs = millis();
+    while (digitalRead(PIN_BUTTON) == HIGH && getVoltage(A0) >= STOP_DISCHARGE_VOLTAGE){
+      digitalWrite(PIN_RELAY_OR_MOSFET, HIGH);
       uint32_t TimeElapsed = millis() - resetTime;
-      display.print(TimeElapsed/1000);                       //Display Time
-      display.display();
+      if ( (millis()-startMillisFormAs) >= millisecFormAsInterval){
+        mAs += (getIntensity(A1)*1000) * (millisecFormAsInterval/1000);
+        startMillisFormAs = millis();
+      }
+      mAh = mAs/3600;
+      //Serial.print(mAs);Serial.print("mas ");Serial.print(mAh);Serial.println("mah ");
+      DisplayRefresh(TimeElapsed/1000, mAh);
     }
   }
 
