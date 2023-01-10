927794cd506f26f4e20cc77a9ce57bb56858aa7f
joyel24 <joel.martin.2104@gmail.com>
2023-01-10 02:43:39 +0100

    Calculation Voltage A0 intensity A1 & Power
    Implemented Voltage on A0,
    Intensity on A1
    Power by combining A0 & A1
    Begin & tests of time implementation for mAh & Wh counter

--- SimpleArduinoBatteryCapacity.ino
+++ SimpleArduinoBatteryCapacity.ino
@@ -15,8 +15,9 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define NUM_OF_AVERAGE_ATTEMPTS 200     //Number of values picked for average adc calculation
-#define MEASURED_VCC_REF 4.6            //Measured voltage accross ground and Vcc pin of your arduino (it can depends on its supply)
+#define NUM_OF_AVERAGE_SAMPLES 2000    //Number of values picked for average adc calculation
+#define MEASURED_VCC_REF 4.75          //Measured voltage accross ground and Vcc pin of your arduino (it can depends on its supply)
+#define SHUNT_RESISTOR_OHM 1.00        //Value of Shunt Resistor (Ohm) to measure intensity of discharge
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,37 +25,55 @@ void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.clearDisplay();
 
-  // Display Welcome Message during 3sec
+  // Display Welcome Message during 2sec
   display.setTextSize(3);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
   display.println("Welcome");
   display.display();
-  delay(3000);
+  delay(2000);
 }
 
-int getadcValue(){
+//Function to get adc value from a desired pin with averaging samples defined by NUM_OF_AVERAGE_SAMPLES
+int getadcValue(int pin){
   uint32_t adcValuesTotal = 0;
-  int averageValue = 0;
-  for (int n = 0; n < NUM_OF_AVERAGE_ATTEMPTS; n++){ //Averaging values for n times attempts
-    adcValuesTotal += analogRead(A0);
+  for (int n = 0; n < NUM_OF_AVERAGE_SAMPLES; n++){ //Averaging values for n sample
+    adcValuesTotal += analogRead(pin);
   }
-  return adcValuesTotal / NUM_OF_AVERAGE_ATTEMPTS;
+  return adcValuesTotal / NUM_OF_AVERAGE_SAMPLES;;
 }
 
-float getADCValToVolts(){
-  float value = getadcValue();
-  return value / 1024 * MEASURED_VCC_REF;
+float getVoltage(int pin){
+  float value = getadcValue(pin);
+  return value / 1023 * MEASURED_VCC_REF;
 }
+
+float getIntesity(int pin){
+  float voltage = getVoltage(pin);
+  return voltage / SHUNT_RESISTOR_OHM;
+}
+
+float getPower(){
+  float voltage = getVoltage(A0);
+  float intensity = getIntesity(A1);
+  float power = (voltage * intensity);
+  return power;
+}
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  //Print Voltage Measured from cell connected to ADC0
+  //Print measured voltage from cell connected
   display.setCursor(0, 0);
-  display.print(getADCValToVolts()); display.print("V");
+  display.print(getVoltage(A0)); display.print("V ");
+  display.print(getIntesity(A1)); display.print("A ");
+  display.print(getPower()); display.print("W ");
+  display.setCursor(0, 9);
+  display.print(millis()/1000);
   display.display();
   //delay(500);
 
